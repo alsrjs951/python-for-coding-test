97e29d50eccbf79a780c05f780c642fd184de8ae
alsrjs951 <62170569+alsrjs951@users.noreply.github.com>
2024-05-14 18:00:50 +0900

    Create 8.py

---
+++ Sorting/8.py
@@ -0,0 +1,24 @@
+# Binary Search (이진 탐색)
+import sys
+
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+# n: 원소의 개수, target: 찾고자 하는 문자열
+n, target = list(map(int, sys.stdin.readline().rstrip().split()))
+array = list(map(int, sys.stdin.readline().rstrip().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result is None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
