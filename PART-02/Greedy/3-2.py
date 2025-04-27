0480f2a8be10391d4499ae276415af4572617b6b
LeeMinGeon <alsrjs951@naver.com>
2025-04-28 01:20:42 +0900

    save

---
+++ PART-02/Greedy/3-2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().rstrip().split())
+numbers = sorted(list(map(int, input().rstrip().split())))
+
+max_number = numbers[n-1]
+second_max_number = numbers[n-2]
+
+max_number_count = m // (k+1) * k
+
+result = max_number_count * max_number + (m - max_number_count) * second_max_number
+
+print(result)
